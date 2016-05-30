375cd460ec1af53cb75ffda0ac6a3a1d1414a12b
Frans Thamura <frans@meruvian.org>
2016-05-30 23:12:22 +0700

    m

--- 3. mainWindow.3/main.js
+++ 3. mainWindow.3/main.js
@@ -1,20 +1,24 @@
 window.onload = function() {
-  let msg = "I'm running Node.js:" + process.version;
-  + ", Electron" + process.versions["electron"]
-  + ", and Chrome" + process.versions["chrome"] ;
+  var foo1 = "chrome";
+  var foo2 = "electron";
+
+   msg = "I'm running Node.js:" + process.version +
+   ", Electron" + process.versions[foo1] +
+   ", and Chrome" + process.versions[foo2] ;
 
   document.querySelector("h1").textContent = msg;
-};
 
-let fs = require("fs");
-let ul = document.querySelector("ul");
-fs.readdir (process.env.HOME, function (err, entries) {
-  entries.forEach(function(entry) {
-    if (!entry.startWith (".")) {
-      let li = document.createElement ("li");
-      li.textContent = entry;
-      ul.appendChild(li);
-    }
-  }
+  var fs = require("fs");
+  var ul = document.querySelector("ul");
+  fs.readdir (process.env.HOME, function (err, entries) {
+    entries.forEach(function(entry) {
+      if (!entry.startWith (".")) {
+        var li = document.createElement ("li");
+        li.textContent = entry;
+        ul.appendChild(li);
+      }
+  });
+
+});
 
 };
